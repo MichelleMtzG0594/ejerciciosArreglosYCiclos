82270996dde37006bcc338bb4d825eb57b82797c
Mich Martínez <michellemtzg.proyectos@gmail.com>
2025-05-24 15:35:31 -0600

    Add: ejercicio1 resuelto

---
+++ ejercicio1.js
@@ -0,0 +1,28 @@
+/*
+EJERCICIO 1 
+let arr = ["This", "is", "a", "sentence."];
+                    function printOutString() {
+                        your code here
+                    }
+                    printOutString();
+                    Complete the function to print out the string: This is a sentence. 
+*/
+
+export{printOutString}
+
+const printOutString = () => {
+    let arr = ["This", "is", "a", "sentence."];
+    let arrJoin = arr.join(" ");
+    return arrJoin;
+}
+console.log(printOutString());
+
+
+const printInDOM = () => {
+    const parrOne = document.getElementById("resultado-ejercicio-1");
+    
+    const result = printOutString()
+    parrOne.innerHTML = `El resultado es: ${result}`
+}
+
+printInDOM()
