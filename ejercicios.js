c9b11dc5423b6a264515ab5ef744bcfa1780370d
Mich Martínez <michellemtzg.proyectos@gmail.com>
2025-05-24 15:34:47 -0600

    Add:estructura básica html,css,js

---
+++ ejercicios.js
@@ -0,0 +1,21 @@
+import { printOutString } from "./ejercicio1.js";
+import { arrayDouble } from "./ejercicio2.js";
+import { multiOfNumbers,sumOfNumbers} from "./ejercicio3.js";
+import { commonCourses } from "./ejercicio4.js";
+import { people,numPeople,printPeople,removeDaniFromPeople,removeJuanfromPeople,moveLuisToTheFront,indexOfMaria,addMyName } from "./ejercicio5.js";
+import { burbuja } from "./ejercicio6.js";
+
+printOutString();
+arrayDouble
+multiOfNumbers
+sumOfNumbers
+commonCourses
+people
+numPeople
+printPeople
+removeDaniFromPeople
+removeJuanfromPeople
+moveLuisToTheFront
+indexOfMaria
+addMyName
+burbuja;
